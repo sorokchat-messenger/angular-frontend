8fe00402ff0e5f6f171424a18ca99c051dd57edc
Sorokovsky <Sorokovsky0809@gmail.com>
2026-09-24 19:56:58 +0300

    feat: disable retrying

--- src/app/providers/tanstack.provider.ts
+++ src/app/providers/tanstack.provider.ts
@@ -1,4 +1,15 @@
 import { type Provider } from "@angular/core";
 import { provideTanStackQuery, QueryClient } from '@tanstack/angular-query-experimental';
 
-export const TANSTACK_PROVIDER: Provider = provideTanStackQuery(new QueryClient());
+export const TANSTACK_PROVIDER: Provider = provideTanStackQuery(new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: false,
+            retryOnMount: false,
+            refetchOnWindowFocus: false
+        },
+        mutations: {
+            retry: true
+        }
+    }
+}));
