e914293b5f4325d66749437bee9cb1820b7256b5
Sorokovsky <Sorokovsky0809@gmail.com>
2026-09-25 15:24:44 +0300

    feat: provide notification into guard

--- src/app/guards/universal.guard.ts
+++ src/app/guards/universal.guard.ts
@@ -3,7 +3,9 @@ import { Path, type Protection } from "@/shared";
 import { inject } from "@angular/core";
 import { toObservable } from "@angular/core/rxjs-interop";
 import { type CanActivateFn, type UrlTree, Router } from "@angular/router";
-import { ROLE_HIERARCHY } from "@sorokchat-messenger/contracts";
+import { TranslateService } from "@ngx-translate/core";
+import { AuthorizationCodes, ROLE_HIERARCHY } from "@sorokchat-messenger/contracts";
+import { toast } from "ngx-sonner";
 import { combineLatest, filter, map, Observable, take } from "rxjs";
 
 export function universalGuard(protection: Protection): CanActivateFn {
@@ -11,6 +13,7 @@ export function universalGuard(protection: Protection): CanActivateFn {
         const isAuthenticatedSignal = injectIsAuthenticated();
         const profile = injectProfile();
         const router: Router = inject(Router);
+        const translation: TranslateService = inject(TranslateService);
         return combineLatest([toObservable(isAuthenticatedSignal), toObservable(profile.data), toObservable(profile.status)])
             .pipe(
                 filter(([isAuthenticated]) => {
@@ -24,17 +27,24 @@ export function universalGuard(protection: Protection): CanActivateFn {
                             return true;
                         }
                         case "anonymous": {
-                            return isAuthenticated ? router.createUrlTree(["/"]) : true;
+                            if (!isAuthenticated) return true;
+                            toast.error(translation.instant(AuthorizationCodes.ACCESS_DENIED));
+                            return router.createUrlTree(["/"]);
                         }
                         case "private": {
-                            return isAuthenticated ? true : router.createUrlTree([Path.LOGIN_PAGE.fullPath]);
+                            if (isAuthenticated) return true;
+                            toast.error(translation.instant(AuthorizationCodes.UNAUTHORIZED));
+                            return router.createUrlTree([Path.LOGIN_PAGE.fullPath]);
                         }
                         case "protected": {
                             if (!user || !isAuthenticated) {
+                                toast.error(translation.instant(AuthorizationCodes.UNAUTHORIZED));
                                 return router.createUrlTree([Path.LOGIN_PAGE.fullPath]);
                             }
                             const hasRoles: boolean = protection.neededRoles.some(needRole => ROLE_HIERARCHY.hasRole(needRole, user.role));
-                            return hasRoles ? true : router.createUrlTree(["/"]);
+                            if (hasRoles) return true;
+                            toast.error(translation.instant(AuthorizationCodes.ACCESS_DENIED));
+                            return router.createUrlTree(["/"]);
                         }
                     }
                 })
--- src/pages/login/data/login.data.ts
+++ src/pages/login/data/login.data.ts
@@ -3,5 +3,5 @@ import { Path, type Page } from "@/shared";
 export const LOGIN_PAGE: Page = {
     loadComponent: () => import('../ui').then(module => module.LoginPage),
     path: Path.LOGIN_PAGE.path,
-    title: "Вхід"
+    title: "Вхід",
 }
